74cfe050ec325757d3caa78128e62c1f28094c02
Maeliss Teissier <maeliss.teissier@gmail.com>
2019-11-16 13:53:48 -0500

    Add EMployee Controller and JSONUtils

---
+++ src/main/java/JSONUtils.java
@@ -0,0 +1,22 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.sf.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static com.google.gson.JsonParser.parseReader;
+
+public class JSONUtils {
+    public static JsonObject getJSONObjectFromURL(String URL) throws Exception{
+        java.net.URL url = new URL(URL);
+        URLConnection request = url.openConnection();
+        request.connect();
+
+        //JsonParser jp = new JsonParser();
+        JsonElement root = parseReader(new InputStreamReader((InputStream) request.getContent()));
+        return root.getAsJsonObject();
+    }
+}
