9357b4eee3a2f3b2dc6522101ec167e2627b4dfe
Corinne Pulgar <corinne@corinnepulgar.com>
2019-11-16 14:21:23 -0500

    Début test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws Exception {
+        Employee employee = Controllers.getEmployee(1);
+        Room office = Controllers.getRoom(3);
     }
 }
