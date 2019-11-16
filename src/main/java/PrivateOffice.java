5527e8cd6701640358042f6784a98608915c3aa1
Blamsart <boisvert-langlois.melanie@courrier.uqam.ca>
2019-11-16 14:46:10 -0500

    Ajout constructeur vide

--- src/main/java/PrivateOffice.java
+++ src/main/java/PrivateOffice.java
@@ -3,16 +3,10 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 
 public class PrivateOffice extends Room {
-
-    int id;
-    String name;
-    int capacity;
-    Thermostat thermo;
-    Television tv;
-    Light light;
-    Phone phone;
-    Speaker speaker;
-    ArrayList<Employee> employees;
+    
+    public PrivateOffice(int id){
+        super(id);
+    }
 
     public PrivateOffice(int id, String name, int capacity, Thermostat thermo, Television tv, Light light, Phone phone, Speaker speaker, ArrayList<Employee> employees) {
         super(id, name, capacity, thermo, tv, light, phone, speaker, employees);
@@ -20,77 +14,4 @@ public class PrivateOffice extends Room {
         this.speaker = null;
     }
 
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public int getCapacity() {
-        return capacity;
-    }
-
-    @Override
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    @Override
-    public Thermostat getThermo() {
-        return thermo;
-    }
-
-    @Override
-    public void setThermo(Thermostat thermo) {
-        this.thermo = thermo;
-    }
-
-    @Override
-    public Light getLight() {
-        return light;
-    }
-
-    @Override
-    public void setLight(Light light) {
-        this.light = light;
-    }
-
-    @Override
-    public Phone getPhone() {
-        return phone;
-    }
-
-    @Override
-    public void setPhone(Phone phone) {
-        this.phone = phone;
-    }
-
-    public ArrayList<Employee> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(ArrayList<Employee> employees) {
-        this.employees = employees;
-    }
-
-    @Override
-    public String toString() {
-        Gson gson = new Gson();
-        return gson.toJson(this);
-    }
 }
