d8f73b1d726ca63ca3d96e64e15fbc9b1379280f
Corinne Pulgar <corinne@corinnepulgar.com>
2019-11-16 17:44:04 -0500

    TESTS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,99 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        int time = 7;
+        //Minuit :
+        int time = 0;
+        moveEmployeesOut();
+        updateAllRooms(time);
 
+        //7h :
+        time = 7;
+        time = passTime(time);
+        updateAllRooms(time);
 
-        Controllers.updateEmployeePosition(7, 3);
-        Controllers.updateEmployeePosition(8, 3);
+        //8h :
+        ArrayList<Integer> employees = new ArrayList<>(Arrays.asList(5,3,7,2));
+        ArrayList<Integer> rooms = new ArrayList<>(Arrays.asList(1,1,2,3));
+        moveEmployees(employees, rooms);
+        updateAllRooms(time);
+        time = passTime(time);
 
-        Room room = Controllers.getRoom(3);
-        System.out.println(room);
+        //9h :
+        employees = new ArrayList<>(Arrays.asList(7,6,3,8,9,11));
+        rooms = new ArrayList<>(Arrays.asList(4,5,4,4,4,4));
+        moveEmployees(employees, rooms);
+        updateAllRooms(time);
+        time = passTime(time);
 
-        if (room.isEmpty()) {
-            room.light.turnOff();
-            System.out.println("Lights off!");
-        } else {
-            room.light.turnOn();
-            System.out.println("Lights on!");
+        //10h :
+        employees = new ArrayList<>(Arrays.asList(8,13,17,9,11,1,14,16));
+        rooms = new ArrayList<>(Arrays.asList(6,6,6,6,6,6,6,6));
+        moveEmployees(employees, rooms);
+        updateAllRooms(time);
+        time = passTime(time);
+
+        //11h :
+        time = passTime(time);
+
+        //12h :
+        employees = new ArrayList<>(Arrays.asList(6,5,8,9,14,11,16,13,17,1,3));
+        rooms = new ArrayList<>(Arrays.asList(3,2,2,2,2,2,2,2,2,2,2));
+        moveEmployees(employees, rooms);
+        updateAllRooms(time);
+        time = passTime(time);
+
+        //13h :
+        employees = new ArrayList<>(Arrays.asList(5,8,9,14,11,16,6,13,19,17,15,1,3));
+        rooms = new ArrayList<>(Arrays.asList(1,4,4,4,4,6,6,6,6,6,6,6,6));
+        moveEmployees(employees, rooms);
+        updateAllRooms(time);
+        time = passTime(time);
+
+        //14h :
+        employees = new ArrayList<>(Arrays.asList(16,13,17,8,9,14,11));
+        rooms = new ArrayList<>(Arrays.asList(2,3,5,4,4,4,4));
+        moveEmployees(employees, rooms);
+        updateAllRooms(time);
+        time = passTime(time);
+
+        //15h : ON CHANGE PAS LES GENS MAIS LA COULEUR
+        time = passTime(time);
+
+        //16h :
+        time = passTime(time);
+
+        //17h :
+        employees = new ArrayList<>(Arrays.asList(13, 11,17, 1,3,19));
+        rooms = new ArrayList<>(Arrays.asList(2, 1, 1,1,1,1));
+        moveEmployees(employees, rooms);
+        updateAllRooms(time);
+        time = passTime(time);
+
+        //18h :
+        moveEmployeesOut();
+        updateAllRooms(time);
+        time = passTime(time);
+    }
+
+    public static void updateAllRooms(int time) throws Exception {
+        for (int i = 1; i <= 6; i++) {
+            Room room = Controllers.getRoom(i);
+
+            if (room.isEmpty()) {
+                room.light.turnOff();
+                System.out.println("Lights off!");
+            } else {
+                room.light.turnOn();
+                System.out.println("Lights on!");
+            }
+            Controllers.updateLight(room.light.isTurnedOn(), Light.getDayColor(time), room.light.getId());
+            System.out.println(room);
         }
-        Controllers.updateLight(room.light.isTurnedOn(), room.light.getHexColor(), room.light.getId());
-
-
-        Controllers.updateEmployeePosition(7, 2);
-        Controllers.updateEmployeePosition(8, 2);
-
-        room = Controllers.getRoom(3);
-        System.out.println(room);
-
-        if (room.isEmpty()) {
-            room.light.turnOff();
-            System.out.println("Lights off!");
-        } else {
-            room.light.turnOn();
-            System.out.println("Lights on!");
-        }
-
-        Controllers.updateLight(room.light.isTurnedOn(), room.light.getHexColor(), room.light.getId());
-
-        room = Controllers.getRoom(3);
-        System.out.println(room);
-
     }
 
     public static void showAllRooms() throws Exception {
@@ -52,15 +103,24 @@ public class Main {
         }
     }
 
-    public static void moveEmployees(ArrayList<Employee> employees, ArrayList<Room> destinations) throws Exception {
+    public static void moveEmployees(ArrayList<Integer> employees, ArrayList<Integer> destinations) throws Exception {
         assert(employees.size() == destinations.size());
 
         for (int i = 0; i < employees.size(); i++) {
-            Controllers.updateEmployeePosition(employees.get(i).getId(), destinations.get(i).id);
+            Controllers.updateEmployeePosition(employees.get(i), destinations.get(i));
         }
     }
 
-    public static void moveEmployeesOut() {
-        ArrayList<Employee> employees = 
+    public static void moveEmployeesOut() throws Exception {
+        for (int i = 1; i <= 19; i++) {
+            Controllers.updateEmployeePosition(i, 1);
+        }
+        System.out.println("Everyone's out!");
+    }
+
+    public static int passTime(int time) {
+        time ++;
+        System.out.println("It's " + time + "H00");
+        return time;
     }
 }
